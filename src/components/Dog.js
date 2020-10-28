3fdeb03d75ec6bd764c835e040080c328b885026
Oksana Sam <oksana.o.samokhvalova@gmail.com>
2020-10-27 23:22:18 -0400

    added paginated query request with a new dog api

--- src/components/Dog.js
+++ src/components/Dog.js
@@ -1,9 +1,12 @@
 import React from "react";
 
-const Dog = ({ breed }) => {
+const Dog = ({ dog }) => {
   return (
     <div className="card">
-      <h3>{breed}</h3>
+      <h3>{dog.breeds[0].name}</h3>
+      <h4>Temperament: {dog.breeds[0].temperament}</h4>
+      <h5>Bred for: {dog.breeds[0].bred_for}</h5>
+      <img src={dog.url} alt={dog.breeds[0].name} />
     </div>
   );
 };
--- src/components/Dogs.js
+++ src/components/Dogs.js
@@ -1,15 +1,28 @@
 import React, { useState } from "react";
-// import { usePaginatedQuery } from "react-query";
-import { useQuery } from "react-query";
+import { usePaginatedQuery } from "react-query";
+// import { useQuery } from "react-query";
 import Dog from "./Dog";
 
-const fetchDogs = async () => {
-  const res = await fetch("https://dog.ceo/api/breeds/list/all");
+const fetchMoreDogs = async (key, page) => {
+  const res = await fetch(
+    `https://api.thedogapi.com/v1/images/search?size=med&mime_types=jpg&format=json&has_breeds=true&order=RANDOM&page=${page}&limit=5`
+  );
   return res.json();
 };
 
+// const fetchDogs = async () => {
+//   const res = await fetch("https://dog.ceo/api/breeds/list/all");
+//   return res.json();
+// };
+
 const Dogs = () => {
-  const { data, status } = useQuery("breeds", fetchDogs);
+  // const { data, status } = useQuery("breeds", fetchDogs);
+
+  const [page, setPage] = useState(1);
+  const { resolvedData, latestData, status } = usePaginatedQuery(
+    ["moredogs", page],
+    fetchMoreDogs
+  );
 
   return (
     <div>
@@ -19,11 +32,24 @@ const Dogs = () => {
 
       {status === "error" && <div>Error fetching data</div>}
 
-      {status === "success" && data.message ? (
+      {status === "success" ? (
         <div>
-          {Object.keys(data.message).map((breed, index) => (
+          <button
+            onClick={() => setPage((old) => Math.max(old - 1, 1))}
+            disabled={page === 1}
+          >
+            Previous Page
+          </button>
+          <span>{page}</span>
+          <button onClick={() => setPage((old) => old + 1)}>Next page</button>
+          <div>
+            {resolvedData.map((dog) => (
+              <Dog key={dog.id} dog={dog} />
+            ))}
+          </div>
+          {/* {Object.keys(data.message).map((breed, index) => (
             <Dog key={index} breed={breed} />
-          ))}
+          ))} */}
         </div>
       ) : null}
     </div>
@@ -31,55 +57,3 @@ const Dogs = () => {
 };
 
 export default Dogs;
-
-// const fetchDogs = async (key, page) => {
-//   const res = await fetch(`http://swapi.dev/api/planets/?page=${page}`);
-//   return res.json();
-// };
-
-// const Dogs = () => {
-//   const [page, setPage] = useState(1);
-//   const { resolvedData, latestData, status } = usePaginatedQuery(
-//     ["planets", page],
-//     fetchPlanets
-//   );
-
-//   return (
-//     <div>
-//       <h2>Planets</h2>
-
-//       {status === "loading" && <div>Loading data</div>}
-
-//       {status === "error" && <div>Error fetching data</div>}
-
-//       {status === "success" && (
-//         <>
-//           <button
-//             onClick={() => setPage((old) => Math.max(old - 1, 1))}
-//             disabled={page === 1}
-//           >
-//             Previous Page
-//           </button>
-//           <span>{page}</span>
-//           <button
-//             onClick={() =>
-//               setPage((old) =>
-//                 !latestData || !latestData.next ? old : old + 1
-//               )
-//             }
-//             disabled={!latestData || !latestData.next}
-//           >
-//             Next page
-//           </button>
-//           <div>
-//             {resolvedData.results.map((planet) => (
-//               <Planet key={planet.name} planet={planet} />
-//             ))}
-//           </div>
-//         </>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default Planets;
